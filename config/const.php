eea8468dc4c5355f75bd4a48d532124908bfa731
Ryota2680 <teruikkmr0826@yahoo.co.jp>
2021-05-23 14:43:55 +0900

    課題33完了

---
+++ config/const.php
@@ -0,0 +1,7 @@
+<?php
+return[
+    //管理画面用のクッキー名称、セッションテーブル名
+    // 'session_cookie_admin' => env('SESSION_COOKIE_ADMIN', Str::slug(env('APP_NAME', 'Laravel'), '_').'_session'),
+    // 'session_cookie_admin' => env('SESSION_COOKIE_ADMIN', Str::slug(env('APP_NAME', 'Laravel'), '_').'_session'),
+    'session_cookie_admin' => env('SESSION_COOKIE_ADMIN'),
+];
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,38 @@ Auth::routes();
 
 Route::get('/', 'ItemController@index')->name('index');
 Route::get('item/detail/{id}', 'ItemController@detail')->name('item.detail');
+
+
+/*
+|--------------------------------------------------------------------------
+| 2) User ログイン後
+|--------------------------------------------------------------------------
+*/
+// Route::group(['middleware' => 'auth:user'], function() {
+//     Route::get('/home', 'HomeController@index')->name('home');
+// });
+
+
+/*
+	|--------------------------------------------------------------------------
+	|  Admin 認証不要
+	|--------------------------------------------------------------------------
+ */
+Route::group(['prefix' => 'admin'], function() {
+	// Route::get('/', function () { return redirect('/admin/home'); });
+	//ログインしていない時にadmin/にログインした場合('/admin/bbbb')に飛ぶ
+	// Route::get('/', function () { return redirect('/admin/bbbb'); });
+	Route::get('/', function () { return redirect('/admin/login'); });
+	Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+	Route::post('login', 'Admin\LoginController@login');
+});
+
+/*
+|--------------------------------------------------------------------------
+| 4) Admin ログイン後
+|--------------------------------------------------------------------------
+*/
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
+    Route::post('logout',   'Admin\LoginController@logout')->name('admin.logout');
+    Route::get('home',      'Admin\HomeController@index')->name('admin.home');
+});
